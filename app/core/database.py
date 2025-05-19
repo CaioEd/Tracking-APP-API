4bb78b1830361660d52d0b32fd46d08b70eff57a
CaioEd <caioe1201@gmail.com>
2025-05-19 14:13:50 -0300

    idk

--- app/core/database.py
+++ app/core/database.py
@@ -6,12 +6,7 @@ load_dotenv()
 
 class Database:
     def __init__(self):
-        self.client = MongoClient(
-            os.getenv("DATABASE_URL"),
-            username=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
-            authSource='admin'
-        )
+        self.client = MongoClient(os.getenv("DATABASE_URI"))
         self.db = self.client[os.getenv("DATABASE_NAME")]
 
     def get_db(self):
